760c49ef6935b8a2eab206bc8589a728dd62cc2c
Indira devi <indirarao1994@gmail.com>
2024-07-10 07:36:58 +0530

    save

---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+$username = $_POST["username"];
+$email = $_POST["email"];
+echo "Username: " . $username . "<br>";  // Concatenate with dot
+echo "Email: " . $email;                  // Concatenate with dot
+?>
